ae09d13dc9bd3c8250dbd3c18f830419ea905ffa
Valentine <mrexox@yahoo.com>
2018-06-11 13:35:32 +0000

    Changed look in .emacs

---
+++ elpa/sexy-monochrome-theme-20171125.1212/sexy-monochrome-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sexy-monochrome-theme" "20171125.1212" "A sexy dark Emacs >= 24 theme for your sexy code" 'nil :commit "529a1c75196391d7f2f1f42cf9d9f4a52c7d9007" :keywords '("themes") :url "https://github.com/voloyev/sexy-monochrome-theme")
