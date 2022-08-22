8f5dc13f17848b99da70ab6dfec5553b2aec6351
Valentin Kiselev <mrexox@evilmartians.com>
2022-08-22 16:20:29 +0300

    sync.sh: Update

--- sync.sh
+++ sync.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-cp -R ~/.config/nvim ./.config
+cp -R ~/.config/sxhkd ./.config/
+cp -R ~/.config/bspwm ./.config/
+cp -R ~/.config/nvim ./.config/
 cp ~/.emacs .emacs
 cp ~/.vimrc .vimrc
 cp ~/.gitignore .gitignore
